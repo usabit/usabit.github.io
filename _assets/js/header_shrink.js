c47a63f7803e87e0d18993d0fb851c67291f2821
cassio mansini <cassiomansini@gmail.com>
2016-11-01 18:37:44 -0200

    ajustes responsive no banner e port

--- _assets/js/header_shrink.js
+++ _assets/js/header_shrink.js
@@ -11,7 +11,7 @@ $(window).scroll(function(){
         if($('.site-header.homepage').data('size') == 'big')
         {
             $('.site-header.homepage').data('size','small');
-            $('.site-header.homepage').stop().animate({ padding:'20px' }, { queue: false, duration: duration });
+            $('.site-header.homepage').stop().animate({ padding:'20px 0' }, { queue: false, duration: duration });
             $('.site-header.homepage .site-title img').stop().animate({width:'30px', marginRight: '10px'}, { queue: false, duration: duration });
             $('.site-header.homepage .site-title svg#usabit-logotipo').stop().animate({width:'80px'}, { queue: false, duration: duration });
         }
@@ -21,7 +21,7 @@ $(window).scroll(function(){
         if($('.site-header.homepage').data('size') == 'small')
         {
             $('.site-header.homepage').data('size','big');
-            $('.site-header.homepage').stop().animate({padding:'50px 20px'}, { queue: false, duration: duration });
+            $('.site-header.homepage').stop().animate({padding:'50px 0'}, { queue: false, duration: duration });
             $('.site-header.homepage .site-title img').stop().animate({width:'60px', marginRight: '18px'}, { queue: false, duration: duration });
             $('.site-header.homepage .site-title svg#usabit-logotipo').stop().animate({width:'120px'}, { queue: false, duration: duration });
         }  
