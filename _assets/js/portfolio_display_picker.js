7f18a3fe6b032c54692a580c3f9a199a71da42be
cassio mansini <cassiomansini@gmail.com>
2016-11-02 23:34:17 -0200

    banner de portfolio

---
+++ _assets/js/portfolio_display_picker.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.portfolio-item a').on('click', function(event){
+    event.preventDefault();
+
+    $('.portfolio-item').removeClass('active');
+    $('.portfolio-display-item').removeClass('active');
+
+    $(this).addClass('active');
+    $('.portfolio-display-item#'+$(this).data('banner')).addClass('active');
+  });
+});
