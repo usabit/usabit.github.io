d3b40e884a1ab2d0a625f7ff387ae6a3362c86a9
cassio mansini <cassiomansini@gmail.com>
2016-11-17 19:05:28 -0200

    banners home

---
+++ _assets/js/owl.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    $("#owl-demo").owlCarousel({
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 100,
+        paginationSpeed: 400,
+        autoPlay: true,
+        items : 1 ,
+        stopOnHover: true
+    });
+
+    console.log('b');
+});
