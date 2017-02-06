9d3ccf510b8f8bd28c50ec1ec26e75d0eb8ecda8
cassio mansini <cassiomansini@gmail.com>
2017-02-06 12:02:18 -0200

    alteracoes carolina

--- _assets/js/portfolio_display_picker.js
+++ _assets/js/portfolio_display_picker.js
@@ -2,10 +2,10 @@ $(function(){
   $('.portfolio-item a').on('click', function(event){
     event.preventDefault();
 
-    $('.portfolio-item a').removeClass('active');
+    $('.portfolio-item').removeClass('active');
     $('.portfolio-display-item').removeClass('active');
 
-    $(this).addClass('active');
+    $(this).parent().addClass('active');
     $('.portfolio-display-item#'+$(this).data('banner')).addClass('active');
   });
 });
