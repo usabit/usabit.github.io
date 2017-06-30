2acaef7af365dfd27b9abc78bce964bba5b553e0
Daniel Lima <daniellima.web@gmail.com>
2017-06-29 22:56:09 -0300

    gerador de sprites

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var rupture = require('rupture');
 var nib = require('nib');
 var prefixer = require('autoprefixer-stylus');
 var sourcemaps = require('gulp-sourcemaps');
+var spritesmith = require('gulp.spritesmith');
 var imagemin = require('gulp-imagemin');
 var bower = require('gulp-bower'); // roda bower install por padrão ou algum outro comando do bower pelo gulp    
 var wiredep = require('wiredep').stream; // aplica os arquivos do bower diretamente na index.html do sistema
@@ -18,7 +19,7 @@ var notify = require('gulp-notify');
 var mainBowerFiles = require('gulp-main-bower-files');
 
 gulp.task('stylus', function() {
-    return gulp.src('_assets/styl/main.styl')
+    return gulp.src('./_assets/styl/main.styl')
         .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
         .pipe(sourcemaps.init())
         .pipe(stylus({
@@ -27,8 +28,27 @@ gulp.task('stylus', function() {
         }))
         //write('.') para jogar o mapa do css na mesma pasta em que vai ser colocado o css gerado
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/css'))
+        .pipe(gulp.dest('./assets/css'))
     ;
+});
+
+gulp.task('sprite', function() {
+    var spriteData =
+        gulp.src('./_assets/img/sprite/*.*')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'sprite.styl',
+            cssFormat: 'stylus',
+            algorithm: 'binary-tree',
+            padding: 3,
+            cssTemplate: 'stylus.template.mustache',
+            cssVarMap: function(sprite) {
+                sprite.name = sprite.name
+            }
+        }));
+
+    spriteData.img.pipe(gulp.dest('./assets/img/'));
+    spriteData.css.pipe(gulp.dest('./_assets/styl/_definitions/'));
 });
 
 gulp.task('reload', function() {
@@ -37,37 +57,38 @@ gulp.task('reload', function() {
 });
 
 gulp.task('js', function() {
-    return gulp.src('_assets/js/**/*.js')
+    return gulp.src('./_assets/js/**/*.js')
         .pipe(plumber())
         .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('assets/js/'))
+        .pipe(gulp.dest('./assets/js/'))
         .on('error', notify.onError({ Title: 'JSHint', message: 'Errors on javascript.' }))
     ;
 });
 
 gulp.task('favicon', function() {
-    return gulp.src('_assets/favicon/**/*')
+    return gulp.src('./_assets/favicon/**/*')
         .pipe(plumber())
-        .pipe(gulp.dest('assets/favicon/'));
+        .pipe(gulp.dest('./assets/favicon/'));
 });
 
 gulp.task('imagemin', ['favicon'], function() {
-    return gulp.src('_assets/img/**/*')
+    return gulp.src('./_assets/img/**/*')
         .pipe(plumber())
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('assets/img/'));
+        .pipe(gulp.dest('./assets/img/'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('_assets/styl/**/*.styl', ['stylus']);
-    gulp.watch('_assets/js/**/*.js', ['js']);
-    gulp.watch('_assets/img/**/*.{jpg,png,gif}', ['imagemin']);
+    gulp.watch('./_assets/styl/**/*.styl', ['stylus']);
+    gulp.watch('./_assets/img/sprite/*.*', ['sprite']);
+    gulp.watch('./_assets/js/**/*.js', ['js']);
+    gulp.watch('./_assets/img/**/*.{jpg,png,gif}', ['imagemin']);
     gulp.watch(['_site/**/*.html', '_site/**/*.css', '_site/**/*.js'], ['reload']);
 });
 
 gulp.task('clean', function(cb) {
-    return del(['_site/', 'assets/', 'bower_components/']);
+    return del(['_site/', './assets/', 'bower_components/']);
 });
 
 gulp.task('bower-install', function() {
@@ -98,7 +119,7 @@ gulp.task('bower', ['bower-install'], function() {
                 }
             }
         }))
-        .pipe(gulp.dest('assets/libs'));
+        .pipe(gulp.dest('./assets/libs'));
 });
 
 gulp.task('wiredep', function() {
@@ -114,7 +135,7 @@ gulp.task('wiredep', function() {
 gulp.task('sequence', function(callback) {
     sequence(
         'bower', 
-        ['js', 'stylus', 'imagemin'],
+        ['sprite', 'stylus', 'imagemin', 'js'],
         'wiredep',
         'watch',
         callback
