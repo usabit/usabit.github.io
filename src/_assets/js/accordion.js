901f7a48f1835b37d1fbe4cb1cb54c6b24c10a88
Daniel Lima <daniellima.web@gmail.com>
2017-12-06 18:23:07 -0200

    adjustments

--- src/_assets/js/accordion.js
+++ src/_assets/js/accordion.js
@@ -2,6 +2,8 @@
     'use strict';
 
     // accordion
+    $('.accordion-item:first-of-type').addClass('active');
+
     $('.accordion-click').on('click', function (e) {
         e.preventDefault();
         $('.accordion-click').parent().not($(this).parent()).removeClass('active');
